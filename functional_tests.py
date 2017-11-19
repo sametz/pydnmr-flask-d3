2b66a46c47bd5e60d20d9b6a1409b312413e970a
Geoffrey M. Sametz <sametz@udel.edu>
2017-11-19 17:04:02 -0500

    functional_tests.py now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:5000')
+class AppTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'pydnmr' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_app(self):
+
+        # The user navigates to the home page for the app
+        self.browser.get('http://localhost:5000')
+
+        # The user sees that the app name contains "pydnmr"
+        self.assertIn('pydnmr', self.browser.title)
+
+        # The user sees a radiobutton menu to select the type of calcuation
+        self.fail('finish writing tests')
+        # The user sees data for the selected model displayed in the body of the page
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

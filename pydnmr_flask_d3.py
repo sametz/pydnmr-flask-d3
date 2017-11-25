aea61aca4e29c45229413f76efc613890fd11107
Geoffrey M. Sametz <sametz@udel.edu>
2017-11-24 20:54:51 -0500

    Radio button selections now return JSON of plot data as [[x coordinates], [y coordinates]]

--- pydnmr_flask_d3.py
+++ pydnmr_flask_d3.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import Flask, jsonify, render_template, request
 from model_definitions import dnmr_two_singlets_kwargs, dnmr_AB_kwargs
 
@@ -12,8 +13,22 @@ def hello_world():
 
 @app.route('/data', methods=['GET', 'POST'])
 def serve_data():
-    model = request.args.get('model')
-    return jsonify(model_dict[model]['entry_dict'])
+    model_arg = request.args.get('model')
+    model = model_dict[model_arg]
+    variable_list = model['entry_names']
+    args = [model['entry_dict'][variable]['value']
+              for variable in variable_list]
+    print(args)
+    plot_data = model['model'](*args)
+    print(plot_data)
+    print(type(plot_data))
+    print(type(plot_data[0]))
+    plot_data_list = [t for t in plot_data]
+    export_data = [[x for x in t] for t in plot_data_list]
+    print(type(export_data))
+    print(type(export_data[0]))
+    print(export_data)
+    return jsonify(export_data)
 
 
 if __name__ == '__main__':
